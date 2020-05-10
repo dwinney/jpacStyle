29e0171d762c0144f9cecbbe12fa7ad402e75c16
dwinney <dwinney@iu.edu>
2020-05-10 02:33:38 -0400

    moved legend down a smidge ;)

--- examples/bessel.cpp
+++ examples/bessel.cpp
@@ -47,7 +47,7 @@ int main()
   }
 
   // Choose the relative coordinates of the bottom right corner of legend
-  plotter->SetLegend(.45, .75);
+  plotter->SetLegend(.45, .70);
 
   // Set the label and range of x axis
   plotter->SetXaxis("W  [GeV]", 0., 20.);
