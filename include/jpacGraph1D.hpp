1f302d363a2ce488f29ec94949c05b71ae46bf6a
dwinney <dwinney@iu.edu>
2020-05-14 23:03:00 -0400

    moved ROOT_italics() and such to plotter.hpp

--- include/jpacGraph1D.hpp
+++ include/jpacGraph1D.hpp
@@ -16,12 +16,6 @@
 #include <tuple>
 
 // -----------------------------------------------------------------------------
-// Super simple string functions to facilitate bolding and italics
-std::string ROOT_italics(std::string in);
-std::string ROOT_bold(std::string in);
-std::string ROOT_bold_italics(std:: string in);
-
-// -----------------------------------------------------------------------------
 // Abstract class for the jpac style
 class jpacGraph1D : public jpacPlotter
 {
